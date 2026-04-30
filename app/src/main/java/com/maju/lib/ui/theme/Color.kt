9f6227eeba3fa674a2fd873102425c7ed13985ea
MajuFacirolli <majufacirolli2004@hotmail.com>
2026-04-30 12:38:55 -0300

    feat: add ui colors

--- app/src/main/java/com/maju/lib/ui/theme/Color.kt
+++ app/src/main/java/com/maju/lib/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// App Colors
+val PrimaryPurple = Color(0xFF6650A4)
+val LightPurple = Color(0xFFD0BCFF)
+val DarkPurple = Color(0xFF21005D)
+val BackgroundLight = Color(0xFFF6F1FF)
+val White = Color(0xFFFFFFFF)
